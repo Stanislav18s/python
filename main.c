56da863ddbd7f44eb69c1fad872c8b0086141406
Stanislav18s <polet18stones@gmail.com>
2025-07-01 20:06:27 +0500

    new repository

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int b = 0;
+scanf ("%d",&b);
+if (b==15)
+prinf("hello world");
+else 
+prinf("yes hello world");
+
+return 0;
+}
---
+++ ru.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+int b =0;
+scanf("%d",&b);
+if(!b)
+printf("b+_l test");
+ else
+printf("b!=0");
+return 0;
+}
